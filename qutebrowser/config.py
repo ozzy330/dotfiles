af5b05bdf379de04e364b2dbcd0a79fe2f4ceab6
ozzy330 <oskar.quesada30@gmail.com>
2025-01-21 18:59:19 -0600

    Add qutebrowser config

---
+++ qutebrowser/config.py
@@ -0,0 +1,18 @@
+config.load_autoconfig(False)
+config.set('colors.webpage.preferred_color_scheme', 'dark')
+
+import os
+from urllib.request import urlopen
+
+# load your autoconfig, use this, if the rest of your config is empty!
+config.load_autoconfig()
+
+if not os.path.exists(config.configdir / "theme.py"):
+    theme = "https://raw.githubusercontent.com/catppuccin/qutebrowser/main/setup.py"
+    with urlopen(theme) as themehtml:
+        with open(config.configdir / "theme.py", "a") as file:
+            file.writelines(themehtml.read().decode("utf-8"))
+
+if os.path.exists(config.configdir / "theme.py"):
+    import theme
+    theme.setup(c, 'mocha', True)
