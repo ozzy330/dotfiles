33e3e6f2b948799a197bf0a84067212e4faaf8e7
ozzy330 <oskar.quesada30@gmail.com>
2025-01-22 22:09:41 -0600

    Add some new aliases

--- set-files.sh
+++ set-files.sh
@@ -2,6 +2,6 @@ DOTSPATH=$HOME/Documents/dotfiles
 
 ln -s $DOTSPATH/nvim ~/.config/nvim
 
-ln -s $DOTSPATH/shell/.bashrc ~/.bashrc
+ln -s $DOTSPATH/shell/bashrc ~/.bashrc
 
 ln -s $DOTSPATH/waybar ~/.config/waybar
