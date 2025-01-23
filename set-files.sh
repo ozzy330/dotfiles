55f907f8874acd7ed285efce6beb5fd742a075dc
ozzy330 <oskar.quesada30@gmail.com>
2025-01-22 23:07:57 -0600

    Add better way to apply configs

--- set-files.sh
+++ set-files.sh
@@ -1,7 +1,19 @@
 DOTSPATH=$HOME/Documents/dotfiles
 
-ln -s $DOTSPATH/nvim ~/.config/nvim
+declare -A conf
+conf["nvim"]="$HOME/.config/nvim"
+conf["shell/bashrc"]="$HOME/.bashrc"
+conf["waybar"]="$HOME/.config/waybar"
 
-ln -s $DOTSPATH/shell/bashrc ~/.bashrc
-
-ln -s $DOTSPATH/waybar ~/.config/waybar
+for org in "${!conf[@]}"; do
+	dest="${conf[$org]}"
+	if [ -e "$dest" ]; then
+		echo "The config already exist ($dest)"
+	else
+		ln -s "$DOTSPATH/$org" $dest
+		echo "Added config ($dest)"
+	fi
+done
+# ln -s $DOTSPATH/nvim ~/.config/nvim
+# ln -s $DOTSPATH/shell/bashrc ~/.bashrc
+# ln -s $DOTSPATH/waybar ~/.config/waybar
