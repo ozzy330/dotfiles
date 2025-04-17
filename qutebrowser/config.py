6860d645c51706c761a38fc0fa30b9dee4af3711
ozzy330 <oskar.quesada30@gmail.com>
2025-04-17 01:40:02 -0600

    add bitwarden

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -2,8 +2,10 @@ config.load_autoconfig(False)
 c.colors.webpage.preferred_color_scheme = "dark"
 c.input.partial_timeout = 100
 # Avoid accidental tab closes
-config.unbind('d')
-config.bind('D', 'tab-close')
+config.unbind("d")
+config.bind("D", "tab-close")
 
-config.bind('<Ctrl-Shift-m>', 'spawn mpv {clipboard}')
-config.bind('<Ctrl-m>', 'hint all yank')  
+config.bind("<Ctrl-Shift-m>", "spawn mpv {clipboard}")
+config.bind("<Ctrl-m>", "hint all yank")
+
+config.bind("<Ctrl-Shift-l>", "spawn --userscript qute-bitwarden")
